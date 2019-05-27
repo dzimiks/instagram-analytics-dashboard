ded4330710070d6e588372a56c4dd76757576b43
dzimiks <vana997@gmail.com>
2019-05-27 11:41:55 +0200

    Add new charts

--- public/js/charts.js
+++ public/js/charts.js
@@ -264,3 +264,196 @@ Highcharts.chart('gender-of-followers-chart', {
 		}],
 	}]
 });
+
+Highcharts.chart('countries-map-chart', {
+	chart: {
+		map: "worldMap"
+	},
+
+	title: {
+		text: "Countries of Followers"
+	},
+
+	mapNavigation: {
+		enabled: false,
+		enableDoubleClickZoomTo: false
+	},
+
+	colorAxis: {
+		min: 1,
+		max: 1000,
+		type: "logarithmic"
+	},
+
+	series: [{
+		data: [
+			["rs", 1000],
+			["us", 830],
+			["in", 49],
+			["ba", 32],
+			["id", 34],
+			["me", 323],
+			["hr", 424],
+			["ng", 4334],
+			["it", 4],
+			["ru", 5]
+		],
+		// joinBy: ["iso-a2", "countryCode"],
+		name: "Countries of Followers",
+		states: {
+			hover: {
+				color: "#e53935"
+			}
+		},
+		tooltip: {
+			pointFormat: "{point.name}: {point.value} followers"
+		}
+	}]
+});
+
+Highcharts.chart('cities-chart', {
+	chart: {
+		type: "bar"
+	},
+
+	title: {
+		text: "Cities of Followers"
+	},
+
+	xAxis: {
+		categories: ["Belgrade", "NYC", "Madrid", "Paris", "Novi Sad"]
+	},
+
+	yAxis: {
+		min: 0,
+		title: {
+			text: null
+		}
+	},
+
+	tooltip: {
+		pointFormat: "{series.name}: {point.y} <b>({point.percentage:.1f} %)</b>"
+	},
+
+	plotOptions: {
+		bar: {
+			dataLabels: {
+				enabled: true
+			}
+		}
+	},
+
+	legend: {
+		enabled: false
+	},
+
+	series: [{
+		color: "#328FFE",
+		name: "City",
+		data: [597, 203, 165, 89, 12]
+	}]
+});
+
+Highcharts.chart('privacy-chart', {
+	chart: {
+		plotBackgroundColor: null,
+		plotBorderWidth: null,
+		plotShadow: false,
+		type: "pie"
+	},
+
+	title: {
+		text: "Privacy of Followers"
+	},
+
+	tooltip: {
+		pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>"
+	},
+
+	plotOptions: {
+		pie: {
+			allowPointSelect: true,
+			cursor: "pointer",
+			dataLabels: {
+				enabled: true
+			},
+			showInLegend: true
+		}
+	},
+
+	series: [{
+		dataLabels: {
+			color: "white",
+			distance: -35,
+			formatter: function () {
+				if (this.percentage !== 0) {
+					return setNumberPrecision(this.percentage, 2) + "%"
+				}
+			}
+		},
+		name: "Privacy of Followers",
+		colorByPoint: true,
+		innerSize: "50%",
+		data: [{
+			name: "Private",
+			color: "#328FFE",
+			y: 53.4
+		}, {
+			name: "Public",
+			color: "#F6AF6D",
+			y: 46.6
+		}],
+	}]
+});
+
+Highcharts.chart('business-chart', {
+	chart: {
+		plotBackgroundColor: null,
+		plotBorderWidth: null,
+		plotShadow: false,
+		type: "pie"
+	},
+
+	title: {
+		text: "Business vs Non Business Account"
+	},
+
+	tooltip: {
+		pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>"
+	},
+
+	plotOptions: {
+		pie: {
+			allowPointSelect: true,
+			cursor: "pointer",
+			dataLabels: {
+				enabled: true
+			},
+			showInLegend: true
+		}
+	},
+
+	series: [{
+		dataLabels: {
+			color: "white",
+			distance: -35,
+			formatter: function () {
+				if (this.percentage !== 0) {
+					return setNumberPrecision(this.percentage, 2) + "%"
+				}
+			}
+		},
+		name: "Business vs Non Business Account",
+		colorByPoint: true,
+		innerSize: "50%",
+		data: [{
+			name: "Business Accounts",
+			color: "#328FFE",
+			y: 63.2
+		}, {
+			name: "Non Business Accounts",
+			color: "#F6AF6D",
+			y: 36.8
+		}],
+	}]
+});
